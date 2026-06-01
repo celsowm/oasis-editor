c447e04b3d286878f3cf8d3621782c1669c1d8d7
celsowm <celsowm@gmail.com>
2026-06-01 09:13:20 -0300

    chore: cleanup documentation and temporary inspection files

--- scratch/extract_style.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('temp_lorem_inspect/word/styles.xml', encoding='utf-8') as f:
-    content = f.read()
-match = re.search(r'<w:style w:type="table" w:styleId="TableGrid">.*?</w:style>', content)
-if match:
-    print(match.group(0))
-else:
-    print('Not found')
